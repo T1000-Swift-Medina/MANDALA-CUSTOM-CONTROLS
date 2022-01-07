61e8d8bfb0a94edb9921db0e6e5525d2f7011830
Abdulaziz Alfayaa <abdulaziz.alfayaa@tuwaiq.edu.sa>
2022-01-07 22:42:56 +0300

    Release Mandala v2.0.2 Initial highlight view Add the highlight view to the view hierarchy Add the horizontal constraint to the highlight view Update the horizontal constraint of the highlight view upon new selection Set the corner radius of the highlight view Update UI mood selector constraints

--- Mandala/ImageSelector.swift
+++ Mandala/ImageSelector.swift
@@ -22,13 +22,23 @@ class ImageSelector: UIControl {
     
     private func configureViewHierachy(){
         addSubview(selectorStackView)
-        
+        insertSubview(highlightView, belowSubview: selectorStackView)
         NSLayoutConstraint.activate([
             selectorStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
             selectorStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
             selectorStackView.topAnchor.constraint(equalTo: topAnchor),
-            selectorStackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            selectorStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            highlightView.heightAnchor.constraint(equalTo: highlightView.widthAnchor),
+            highlightView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.8),
+            highlightView.centerYAnchor.constraint(equalTo: selectorStackView.centerYAnchor)
         ])
+    }
+    private var highlightViewXConstraint: NSLayoutConstraint! {
+        didSet {
+            oldValue?.isActive = false
+            highlightViewXConstraint.isActive = true
+        }
     }
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -39,7 +49,18 @@ class ImageSelector: UIControl {
         configureViewHierachy()
     }
     
-    var selectedIndex = 0
+    var selectedIndex = 0 {
+        didSet{
+        if selectedIndex < 0 {
+            selectedIndex = 0
+        }
+        if selectedIndex >= imageButtons.count {
+            selectedIndex = imageButtons.count - 1
+        }
+        let imageButton = imageButtons[selectedIndex]
+        highlightViewXConstraint = highlightView.centerXAnchor.constraint(equalTo: imageButton.centerXAnchor)
+        }
+    }
     private var imageButtons: [UIButton] = [] {
         didSet {
             oldValue.forEach({$0.removeFromSuperview()})
@@ -53,7 +74,7 @@ class ImageSelector: UIControl {
                 
                 imageButton.setImage(image, for: .normal)
                 imageButton.imageView?.contentMode = .scaleAspectFit
-                imageButton.adjustsImageSizeForAccessibilityContentSizeCategory = false
+                imageButton.adjustsImageWhenHighlighted = false
                 imageButton.addTarget(self,
                                       action: #selector(imageButtonTapped(_:)), for: .touchUpInside)
                 return imageButton
@@ -61,7 +82,13 @@ class ImageSelector: UIControl {
             selectedIndex = 0
         }
     }
-    
+    private let highlightView: UIView = {
+        let view = UIView()
+        view.backgroundColor = view.tintColor
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
     @objc private func imageButtonTapped(_ sender: UIButton){
         guard let buttonIndex = imageButtons.firstIndex(of: sender) else {
             preconditionFailure("The buttons and images are not parallel")
@@ -70,4 +97,9 @@ class ImageSelector: UIControl {
         sendActions(for: .valueChanged)
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        highlightView.layer.cornerRadius = highlightView.bounds.width / 2
+    }
 }
